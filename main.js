03b07ae046ee5eb99a9a104db63c6346d6f41cf3
Akeda99 <raykevin1999@gmail.com>
2022-11-10 16:22:51 -0500

    fuaaa

--- main.js
+++ main.js
@@ -10,9 +10,11 @@ const Hoodies_products=document.querySelector(".Hoodies_products");
 const content_cartshop=document.querySelector(".content_cartshop");
 const shop_cart=document.querySelector(".shop_cart");
 const content_cartshop_items=document.querySelector(".content_cartshop_items");
+const content_cartshop_total=document.querySelector(".content_cartshop_total");
+const icon_x2=document.querySelector(".icon_x2");
+const countClothes=document.querySelector(".countClothes");   
 
-
-const clothes =[
+let clothes =[
     {
         id:1,
         name:'Hoodies',
@@ -38,10 +40,119 @@ const clothes =[
         quantity:20
     },
     ];
+let objCartShop={};
+
+function deleteClothe(idClothes){
+    const op=confirm("Seguro que quieres eliminar?")
+    if(op){
+       delete objCartShop[idClothes];
+    }
+}
+function countProduct(){
+    const arrayCartShop= Object.values(objCartShop);
+    let suma= arrayCartShop.reduce((acum,curr)=>{
+        acum+= curr.amount;
+        return acum;
+    },0);
+    countClothes.textContent= suma;
+}
+function printTotal(){
+    const arrayCartShop= Object.values(objCartShop);
+
+    if(!arrayCartShop.length) {
+    let html =`
+    <h3> Carrito Vacio </h3>
+    <img src="./assets/empty-cart.png" alt="carrito vacio">
+    `;
+    return (content_cartshop_total.innerHTML=html);
+    }
+        
+
+    let total= arrayCartShop.reduce((acum,curr)=>{
+        acum+= curr.price * curr.amount;
+        return acum;
+    },0);
+    let suma= arrayCartShop.reduce((acum,curr)=>{
+        acum+= curr.amount;
+        return acum;
+    },0);
+    countClothes.textContent= suma;
+    let html= `
+    <h3>Numero de Items:${suma}<h3>
+    <h3>${total}.00<h3>
+    <button class="btn btn_buy">Comprar</button>`
+    content_cartshop_total.innerHTML=html;
+    
+}
+
+function printClothes(){
+    let html="";
+
+clothes.forEach(({id, name, price, image, category, quantity})=> {
+    const btnBuy= quantity 
+    ?`<button class="boton_plus btn_add" id="${id}">+</button>`
+    : `<button class="boton_plus btn_nodrop" id="${id}"></button>`;
+    html+= `
+    <div class="clothes_products">
+        <div class="img_products">
+            <img src="${image}" alt="${category}" class="img_size">
+        </div>
+        <div class="products_data">
+        <h2>$${price}.00<span>| Stock: ${quantity}</span></h2>
+        <h3>${name}</h3>
+        ${btnBuy}
+        </div>
+        </div>
+    
+    `;
+})
+Hoodies_products.innerHTML= html;
+}
+printClothes();
+
+function printClothesInCart(){
+    let html = ''
+    let suma_sub=0
+    const arrayCartShop=Object.values(objCartShop);
+
+    arrayCartShop.forEach( ({id, name, price, image, category, quantity,amount})=>{
+        suma_sub=(Number(amount)*Number(price)),
+        html+=`    <div class="clothes_products_shop">
+        <div class="img_text_shop">
+        <div class="img_shop">
+            <img src="${image}" alt="${category}" class="img_size_shop">
+        </div>
+        <div class="products_data_shop">
+        <h2 class="type_clothe_shop">${name}</h2>
+        <div class="stock_price">
+        <p><span>Stock:${quantity} | </span>$${price}.00 </p>
+        </div>
+        
+        <h3 class="text_subtotal">Subtotal: $${suma_sub}.00</h3>
+        
+        <div class="clothes_options_shop">
+        <button class="btn btn_rest" id="${id}">-</button>
+        <p>${amount} units</p>
+        <button class="btn btn_add1" id="${id}">+</button>
+        <button class="btn btn_del" id="${id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="btn btn_del" id="${id}"viewBox="0 0 16 16">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+      </svg></button>
+     
+        </div>
+        </div>
+        </div>
+        </div>
+        `
+    });
+    content_cartshop_items.innerHTML=html
+    printTotal();
+    countProduct();
+}
+
 window.addEventListener('load', function () {
     load()
   })
-  
 icon_menu.addEventListener("click", function(){
     menu.classList.toggle("menu_show");
     icon_x.classList.toggle("icon_show");
@@ -70,68 +181,12 @@ icon_x.addEventListener("click", function(){
 })
 shop_cart.addEventListener("click",function(){
     content_cartshop.classList.toggle("content_cartshop_show");
+    icon_x2.classList.toggle("icon_show2")
 });
-
-function printClothes(){
-    let html="";
-
-clothes.forEach(({id, name, price, image, category, quantity})=> {
-    html+= `
-    <div class="clothes_products">
-        <div class="img_products">
-            <img src="${image}" alt="${category}" class="img_size">
-        </div>
-        <div class="products_data">
-        <h2>$${price}.00<span>| Stock: ${quantity}</span></h2>
-        <h3>${name}</h3>
-        <button class="boton_plus btn_add" id="${id}">+</button>
-        </div>
-        </div>
-    
-    `;
+icon_x2.addEventListener("click", function(){
+    icon_x2.classList.toggle("icon_show2");
+    content_cartshop.classList.toggle("content_cartshop_show");
 })
-Hoodies_products.innerHTML= html;
-}
-printClothes();
-
-function printClothesInCart(){
-    let html = ''
-
-    const arrayCartShop=Object.values(objCartShop);
-
-    arrayCartShop.forEach( ({id, name, price, image, category, quantity,amount})=>{
-        html+=`    <div class="clothes_products_shop">
-        <div class="img_text_shop">
-        <div class="img_shop">
-            <img src="${image}" alt="${category}" class="img_size_shop">
-        </div>
-        <div class="products_data_shop">
-        <h2 class="type_clothe_shop">${name}</h2>
-        <div class="stock_price">
-        <p><span>Stock:${quantity} | </span>$${price}.00 </p>
-        </div>
-        
-        <h3 class="text_subtotal">Subtotal:</h3>
-        
-        <div class="clothes_options_shop">
-        <button class="btn btn_rest" id="${id}">-</button>
-        <p>${amount} units</p>
-        <button class="btn btn_add1" id="${id}">+</button>
-        <button class="btn btn_del" id="${id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="btn btn_del" id="${id}"viewBox="0 0 16 16">
-        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-      </svg></button>
-     
-        </div>
-        </div>
-        </div>
-        </div>`
-    });
-    content_cartshop_items.innerHTML=html
-}
-
-let objCartShop={};
-
 Hoodies_products.addEventListener('click',(e)=>{
     if(e.target.classList.contains('btn_add')){
 
@@ -140,28 +195,46 @@ Hoodies_products.addEventListener('click',(e)=>{
         const currentClothes=clothes.find((clothes)=>clothes.id===idClothes);
 
         if(objCartShop[currentClothes.id]){
+            if(currentClothes.quantity===objCartShop[idClothes].amount) return alert("No hay mas productos en el stock");
+
             objCartShop[currentClothes.id].amount++;
         }else{
-            objCartShop[currentClothes.id]=currentClothes;
+            objCartShop[currentClothes.id]={... currentClothes};
             objCartShop[currentClothes.id].amount=1;
         }
         printClothesInCart();
+        content_cartshop.classList.toggle("content_cartshop_show");
+        icon_x2.classList.toggle("icon_show2");
     }
 });
 content_cartshop.addEventListener('click',(e)=>{
     
     if(e.target.classList.contains('btn_add1')){
         const idClothes=Number(e.target.id);
+
+        const currentClothes=clothes.find((clothes)=>clothes.id===idClothes);
+
+        if(objCartShop[currentClothes.id]){
+            if(currentClothes.quantity===objCartShop[idClothes].amount) return alert("No hay mas productos en el stock");
+
         objCartShop[idClothes].amount++;
+        }
     }
     if(e.target.classList.contains('btn_rest')){
          const idClothes=Number(e.target.id);
-         objCartShop[idClothes].amount--;
+        if(objCartShop[idClothes].amount===1){
+            deleteClothe(idClothes);
+        } else{
+            objCartShop[idClothes].amount--;
+        }
+
+       
     }
     
     if(e.target.classList.contains('btn_del')){
          const idClothes=Number(e.target.id);
-        delete objCartShop[idClothes];
+         deleteClothe(idClothes);
+        
     }
     
     printClothesInCart();
@@ -170,3 +243,25 @@ document.addEventListener('DOMContentLoaded', function () {
     darkTheme()
     headerScroll()
 })
+
+content_cartshop_total.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("btn_buy")){
+        const op= confirm ('Estas seguro de esto?')
+        if(op){
+            clothes = clothes.map((cloth) => {
+                if(objCartShop[cloth.id]?.id===cloth.id){
+                    return {
+                        ... cloth,
+                        quantity: cloth.quantity - objCartShop[cloth.id].amount,
+                    };
+                }else {
+                    return cloth;
+                }
+            });
+         
+            objCartShop={};
+            printClothes();
+            printClothesInCart();
+        }
+    }
+});
